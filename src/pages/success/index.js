db13aa3f81928980324934547cdc5a7bb629af32
Chris Jaeger <chris@seejaeger.com>
2018-10-28 14:41:49 -0400

    success page for contact form netlify redirect

---
+++ src/pages/success/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+import {Container, Divider, Grid, Section, Header, Message } from 'semantic-ui-react'
+
+const SuccessPage = () => (
+    <Layout>
+        <Grid centered>
+            <Container>
+                Thanks for reaching out!
+                Click here to return to the home page or something like that
+                What does netlify's success message look like?
+            </Container>
+        </Grid>
+    </Layout>
+)
