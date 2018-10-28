4b398d4547517f7fe6d863ce6912db3f734b4c2d
Chris Jaeger <chris@seejaeger.com>
2018-10-28 14:43:10 -0400

    contact form updates needs live testing

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,33 +1,72 @@
 import React from 'react'
 import Layout from '../../components/Layout'
-import {Button, Form, TextArea } from 'semantic-ui-react'
+import {
+    Button, 
+    Container,
+    Form, 
+    Grid,
+    Header,
+    Message,
+    Segment,
+    TextArea } from 'semantic-ui-react'
 
 const ContactPage = () => (
     <Layout>
-        <div>
-            <h1>Contact</h1>
-            <p>Holler at me</p>
-            <Form netlify-honeypot='bot-field' netlify>
-                <Form.Field>
-                    <label>Are you a Russian bot?</label>
-                    <input name='bot-field'/>
-                </Form.Field>
-                <Form.Field>
-                    <label>Name</label>
-                    <input placeholder='Name' />
-                </Form.Field>
-                <Form.Field>
-                    <label>Email</label>
-                    <input placeholder='your@email.com' />
-                </Form.Field>
-                <Form.Field>
-                    <label>Message</label>
-                    <TextArea placeholder='What would you like to say?'/>
-                </Form.Field>
-                <Button type='submit'>Submit</Button>
-            </Form>
-        </div>
+        <Grid centered verticalAlign='middle' style={{marginBottom: '5em'}}>
+            <Grid.Row>
+                <Grid.Column>
+                    <Container textAlign='center'>
+                        <Header as='h2' size='huge'>
+                            Let's get in touch
+                            <Header.Subheader>Fill out the form below or reach out on one of these platforms:</Header.Subheader>
+                        </Header>
 
+                        <ul style={{display: 'inline-block'}}>
+                            <li>LinkedIn</li>
+                            <li>Twitter</li>
+                            <li>StackOverflow</li>
+                            <li>GitHUb</li>
+                            <li>Codepen</li>
+                        </ul>
+                    </Container>
+                </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+                <Grid.Column>
+                    <Container>
+                        {/* Add action='pages/success' to redirect form submit
+                        https://www.netlify.com/docs/form-handling/ */}
+                        <Segment>
+                            <Form name='chrisjaeger.me contact' netlify-honeypot='bot-field' netlify>
+                                <Form.Field hidden>
+                                    <label>Are you a Russian bot?</label>
+                                    <input name='bot-field'/>
+                                </Form.Field>
+                                <Form.Field required>
+                                    <label>Name</label>
+                                    <input placeholder='Name' />
+                                </Form.Field>
+                                <Form.Field required>
+                                    <label>Email</label>
+                                    <input placeholder='your@email.com' />
+                                </Form.Field>
+                                <Form.Field required>
+                                    <label>Message</label>
+                                    <TextArea placeholder='What would you like to say?'/>
+                                </Form.Field>
+                                <Message success header='Form Completed' content="You're all signed up for the newsletter" />
+                                <Message
+                                    error
+                                    header='Action Forbidden'
+                                    content='You can only sign up for an account once with a given e-mail address.'
+                                />
+                                <Button type='submit'>Submit</Button>
+                            </Form>
+                        </Segment>
+                    </Container>
+                </Grid.Column>
+            </Grid.Row>
+        </Grid>
     </Layout>
 )
 
