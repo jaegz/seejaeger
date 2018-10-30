a5d6d359458b3b25b098d6b633e7587a0889b5cf
Chris Jaeger <chris@seejaeger.com>
2018-10-30 11:39:15 -0400

    remove unused imports for failiing netlify build

--- src/pages/success/index.js
+++ src/pages/success/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Layout from '../../components/Layout'
-import {Container, Divider, Grid, Section, Header, Message } from 'semantic-ui-react'
+import {Container, Grid } from 'semantic-ui-react'
 
 const SuccessPage = () => (
     <Layout>
