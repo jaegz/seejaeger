2a5ddc935a7f28f5b95bbb88c81fb5c45491ca3c
cjaeger <chris@seejaeger.com>
2018-11-06 10:50:20 -0500

    stateful form with submit redirect to success page

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { navigateTo } from 'gatsby-link';
 import Layout from '../../components/Layout'
 import {
     Button, 
@@ -10,64 +11,167 @@ import {
     Segment,
     TextArea } from 'semantic-ui-react'
 
-const ContactPage = () => (
-    <Layout>
-        <Grid centered verticalAlign='middle' style={{marginBottom: '5em'}}>
-            <Grid.Row>
-                <Grid.Column>
-                    <Container textAlign='center'>
-                        <Header as='h2' size='huge'>
-                            Let's get in touch
-                            <Header.Subheader>Fill out the form below or reach out on one of these platforms:</Header.Subheader>
-                        </Header>
+function encode(data) {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
+}
 
-                        <ul style={{display: 'inline-block'}}>
-                            <li>LinkedIn</li>
-                            <li>Twitter</li>
-                            <li>StackOverflow</li>
-                            <li>GitHUb</li>
-                            <li>Codepen</li>
-                        </ul>
-                    </Container>
-                </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-                <Grid.Column>
-                    <Container>
-                        {/* Add action='pages/success' to redirect form submit
-                        https://www.netlify.com/docs/form-handling/ */}
-                        <Segment>
-                            <Form name='chrisjaeger.me contact' method="POST" netlify-honeypot='bot-field' data-netlify="true">
-                                <Form.Field hidden>
-                                    <label>Are you a Russian bot?</label>
-                                    <input name='bot-field'/>
-                                </Form.Field>
-                                <Form.Field required>
-                                    <label>Name</label>
-                                    <input placeholder='Name' />
-                                </Form.Field>
-                                <Form.Field required>
-                                    <label>Email</label>
-                                    <input placeholder='your@email.com' />
-                                </Form.Field>
-                                <Form.Field required>
-                                    <label>Message</label>
-                                    <TextArea placeholder='What would you like to say?'/>
-                                </Form.Field>
-                                <Message success header='Form Completed' content="You're all signed up for the newsletter" />
-                                <Message
-                                    error
-                                    header='Action Forbidden'
-                                    content='You can only sign up for an account once with a given e-mail address.'
-                                />
-                                <Button type='submit'>Submit</Button>
-                            </Form>
-                        </Segment>
-                    </Container>
-                </Grid.Column>
-            </Grid.Row>
-        </Grid>
-    </Layout>
-)
+export default class ContactPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
 
-export default ContactPage
+    handleChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+        const form = e.target;
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({
+                "form-name": form.getAttribute("name"),
+                ...this.state
+            })
+        })
+        .then(() => navigateTo(form.getAttribute("action")))
+        .catch(error => alert(error));
+    };
+
+    render() {
+        return (
+            <Layout>
+                <Grid centered verticalAlign='middle' style={{ marginBottom: '5em' }}>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Container textAlign='center'>
+                                <Header as='h2' size='huge'>
+                                    Let's get in touch
+                                    <Header.Subheader>Fill out the form below or reach out on one of these platforms:</Header.Subheader>
+                                </Header>
+
+                                <ul style={{ display: 'inline-block' }}>
+                                    <li>LinkedIn</li>
+                                    <li>Twitter</li>
+                                    <li>StackOverflow</li>
+                                    <li>GitHUb</li>
+                                    <li>Codepen</li>
+                                </ul>
+                            </Container>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Container>
+                                <Segment>
+                                    <Form 
+                                        name='chrisjaeger.me contact' 
+                                        method='post'
+                                        action='/success/'
+                                        data-netlify='true'
+                                        data-netlify-honeypot='bot-field'
+                                        onSubmit={this.handleSubmit}
+                                    >
+                                        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+                                        <input type='hidden' name='form-name' value='chrisjaeger.me contact' />
+                                        
+                                        <Form.Field hidden>
+                                            <label>Don't fill this out:{" "}</label>
+                                            <input name='bot-field' onChange={this.handleChange} />
+                                        </Form.Field>
+                                        <Form.Field required>
+                                            <label>Name</label>
+                                            <input name='name' placeholder='Name' onChange={this.handleChange}/>
+                                        </Form.Field>
+                                        <Form.Field required>
+                                            <label>Email</label>
+                                            <input name='email' placeholder='your@email.com' onChange={this.handleChange}/>
+                                        </Form.Field>
+                                        <Form.Field required>
+                                            <label>Message</label>
+                                            <TextArea name='message' placeholder='What would you like to say?' onChange={this.handleChange} />
+                                        </Form.Field>
+                                        <Message success header='Form Completed' content="You're all signed up for the newsletter" />
+                                        <Message
+                                            error
+                                            header='Action Forbidden'
+                                            content='You can only sign up for an account once with a given e-mail address.'
+                                        />
+                                        <Button type='submit'>Submit</Button>
+                                    </Form>
+                                </Segment>
+                            </Container>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </Layout>
+        )
+    }
+}
+
+
+// const ContactPage = () => (
+//     <Layout>
+//         <Grid centered verticalAlign='middle' style={{marginBottom: '5em'}}>
+//             <Grid.Row>
+//                 <Grid.Column>
+//                     <Container textAlign='center'>
+//                         <Header as='h2' size='huge'>
+//                             Let's get in touch
+//                             <Header.Subheader>Fill out the form below or reach out on one of these platforms:</Header.Subheader>
+//                         </Header>
+
+//                         <ul style={{display: 'inline-block'}}>
+//                             <li>LinkedIn</li>
+//                             <li>Twitter</li>
+//                             <li>StackOverflow</li>
+//                             <li>GitHUb</li>
+//                             <li>Codepen</li>
+//                         </ul>
+//                     </Container>
+//                 </Grid.Column>
+//             </Grid.Row>
+//             <Grid.Row>
+//                 <Grid.Column>
+//                     <Container>
+//                         <Segment>
+//                             <Form name='chrisjaeger.me contact' netlify netlify-honeypot='bot-field'>
+//                                 <Form.Field hidden>
+//                                     <input type='hidden' name='form-name' value='chrisjaeger.me contact'/>
+//                                 </Form.Field>
+//                                 <Form.Field hidden>
+//                                     <input name='bot-field'/>
+//                                 </Form.Field>
+//                                 <Form.Field required>
+//                                     <label>Name</label>
+//                                     <input name='name' placeholder='Name' />
+//                                 </Form.Field>
+//                                 <Form.Field required>
+//                                     <label>Email</label>
+//                                     <input name='email' placeholder='your@email.com' />
+//                                 </Form.Field>
+//                                 <Form.Field required>
+//                                     <label>Message</label>
+//                                     <TextArea name='message' placeholder='What would you like to say?'/>
+//                                 </Form.Field>
+//                                 <Message success header='Form Completed' content="You're all signed up for the newsletter" />
+//                                 <Message 
+//                                     error
+//                                     header='Action Forbidden'
+//                                     content='You can only sign up for an account once with a given e-mail address.'
+//                                 />
+//                                 <Button type='submit'>Submit</Button>
+//                             </Form>
+//                         </Segment>
+//                     </Container>
+//                 </Grid.Column>
+//             </Grid.Row>
+//         </Grid>
+//     </Layout>
+// )
+
+// export default ContactPage
--- src/pages/success/index.js
+++ src/pages/success/index.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import Layout from '../../components/Layout'
-import {Container, Grid } from 'semantic-ui-react'
+import {Container, Header, Grid, Segment } from 'semantic-ui-react'
 
 const SuccessPage = () => (
     <Layout>
         <Grid centered>
             <Container>
-                Thanks for reaching out!
-                Click here to return to the home page or something like that
-                What does netlify's success message look like?
+                <Segment style={{marginTop: '5em'}}>
+                    <Header>
+                        Thanks for reaching out!
+                        <Header.Subheader>Click here to return to the home page or something like that</Header.Subheader>
+                    </Header>
+                </Segment>
             </Container>
         </Grid>
     </Layout>
