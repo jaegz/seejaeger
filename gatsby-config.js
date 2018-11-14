e4abc6fe39b9e4f88efa1da81d2119a484fe33fc
cjaeger <chris@seejaeger.com>
2018-11-14 10:40:09 -0500

    add gatsby google analytics plugin and tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,26 @@ module.exports = {
         modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-129211845-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        //exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Enables Google Optimize using your container Id
+        //optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Any additional create only fields (optional)
+        //sampleRate: 5,
+        //siteSpeedSampleRate: 10,
+        cookieDomain: "chrisjaeger.me",
+      },
+    },
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 }
