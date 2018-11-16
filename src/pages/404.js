7099b7e649dc8068f269c817356a8d972c88038e
cjaeger <chris@seejaeger.com>
2018-11-15 22:12:59 -0500

    make 404 page look pretty

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,44 @@
 import React from 'react'
 import Layout from '../components/Layout'
+// import NavbarBottom from '../components/NavbarBottom'
+import {
+  Container,
+  Grid,
+  Header,
+  Icon,
+  Menu,
+  Segment
+} from 'semantic-ui-react'
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
+    <Grid centered style={{ background: 'url(/img/lostinthedesert.jpg) center', backgroundSize: 'cover', height: '100vh' }}>
+      <Grid.Column mobile={16} tablet={14} computer={8} widescreen={6}>
+        <Container>
+          <Segment textAlign='center'>
+            <Icon name='warning circle' size='huge' />
+            <Header as='h1'>
+              404 : NOT FOUND
+            </Header>
+            <p style={{fontSize: '1.3em'}}>There's nothing here... give one of these pages a try:</p>
+            <Menu icon='labeled' fluid widths={4}>
+              <Menu.Item name='home' href='/' >
+                Home
+              </Menu.Item>
+              <Menu.Item name='portfolio' href='/portfolio'>
+                Portfolio
+              </Menu.Item>
+              <Menu.Item name='blog' href='/blog'>
+                Blog
+              </Menu.Item>
+              <Menu.Item name='contact' href='/contact'>
+                Contact
+              </Menu.Item>
+            </Menu>
+          </Segment>
+        </Container>
+      </Grid.Column>
+    </Grid>
   </Layout>
 )
 
