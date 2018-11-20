153987f2f1187b76971c809764b9c13c008df382
cjaeger <chris@seejaeger.com>
2018-11-19 20:47:30 -0500

    cleanup

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,17 +4,11 @@ import 'semantic-ui-css/semantic.min.css'
 import Navbar from '../components/Navbar'
 import NavbarBottom from '../components/NavbarBottom'
 
-const style = {
-  // paddingTop: '4.3em',
-}
-
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet title="Front End Web Developer Portfolio | chrisjaeger.me" />
-    <Navbar />
-    <div style={style}>
-      {children}
-    </div>
+    <Navbar/>
+    {children}
     <NavbarBottom />
   </div>
 )
--- src/components/SocialButtons.js
+++ src/components/SocialButtons.js
@@ -3,11 +3,11 @@ import { Button } from 'semantic-ui-react';
 
 const SocialButtons = () => (
     <div>
-        <Button circular inverted color='white' size='large' href='https://www.linkedin.com/in/christopher-jaeger/' icon='linkedin' />
-        <Button circular inverted color='white' size='large' href='https://twitter.com/seejaeger' icon='twitter' />
-        <Button circular inverted color='white' size='large' href='https://stackoverflow.com/users/2561435/jaegs' icon='stack overflow' />
-        <Button circular inverted color='white' size='large' href='https://github.com/jaegz' icon='github' />
-        <Button circular inverted color='white' size='large' href='https://codepen.io/jaegs/' icon='codepen' />
+        <Button circular inverted size='large' href='https://www.linkedin.com/in/christopher-jaeger/' icon='linkedin' />
+        <Button circular inverted size='large' href='https://twitter.com/seejaeger' icon='twitter' />
+        <Button circular inverted size='large' href='https://stackoverflow.com/users/2561435/jaegs' icon='stack overflow' />
+        <Button circular inverted size='large' href='https://github.com/jaegz' icon='github' />
+        <Button circular inverted size='large' href='https://codepen.io/jaegs/' icon='codepen' />
     </div>
 )
 
