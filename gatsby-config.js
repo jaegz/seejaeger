dfa0088f897de74d045ef502f573eda4a8d0d1db
cjaeger <chris@seejaeger.com>
2018-11-27 18:38:21 -0500

    load google analytics in the head for search console detection

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,7 +62,7 @@ module.exports = {
       options: {
         trackingId: "UA-129211845-1",
         // Puts tracking script in the head instead of the body
-        head: false,
+        head: true,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
